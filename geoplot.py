ff936a389fe2f6c2b488732eb194a879a30f9615
Matt Widjaja <matthew@wimagine.org>
2017-03-18 22:49:36 -0400

    Replaced Bokeh with Folium
    Using folium now to do geographic plotting since it runs faster, with
    more reliability, without Google APIs

--- geoplot.py
+++ geoplot.py
@@ -8,6 +8,8 @@ from bokeh.io import output_file, save
 from bokeh.models import (GMapPlot, GMapOptions, ColumnDataSource, Circle,
                           DataRange1d, HoverTool, PanTool, ZoomInTool,
                           ZoomOutTool, WheelZoomTool, BoxSelectTool, ResetTool)
+import folium
+import pandas as pd
 
 
 def df_to_cds(data_df, columns):
@@ -74,3 +76,37 @@ def plot_cds_geo(data_cds, map_opts_dict, size_col, out_path,
                        BoxSelectTool(), ResetTool())
     output_file(out_path)
     save(plot)
+
+
+def scatterplot_map(data, map_opts, out_path):
+    """ Creates a folium (Open source tiles) map with circle marker plots, the
+        markers having no interactivity.
+
+        Inputs:
+        data: A dictionary or Pandas series with index col of data.
+              Keys or index must be a tuple of coordinates as (<lat>, <lon>)
+              Values must be how many 'items' are in that coordinate.
+        map_opts: Dict of map settings including these keys:
+                  lat = latitude center of map
+                  lng = longitude center of map
+                  size = A number which will be multipled by the value to
+                         determine the size of the circle marker.
+                  color = HTML color code for the circle
+        out_path: Path to save the HTML plot to
+    """
+    map = folium.Map(location=[map_opts['lat'], map_opts['lng']])
+    if isinstance(data, dict):
+        keys = data.keys()
+    elif isinstance(data, pd.Series):
+        keys = data.index
+    else:
+        print('ERROR: Unknown data type to plot here')
+        keys = []
+
+    for coordinate in keys:
+        lat, lon = coordinate  # (y,x) effectively
+        folium.CircleMarker([lat, lon],
+                            radius=data[coordinate] * map_opts['size'],
+                            popup='Occurances: {}'.format(data[coordinate]),
+                            fill_color=map_opts['color']).add_to(map)
+    map.save(out_path)
